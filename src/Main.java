252d29e104eb97f8b5126cafaf4983e484fb2850
Matvey Doomchev <32887734+doomchev@users.noreply.github.com>
2021-10-06 19:31:02 +1100

    version 0.5
    Block bounds detection.
    Export of sprites (changed block groups).

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
 import javax.imageio.ImageIO;
 
 public class Main {
+  public static Chunk zeroChunk = new Chunk();
+  
   public static class Chunk {
     public final int[] value = new int[8];
     public int quantity = 1;
@@ -57,18 +59,20 @@ public class Main {
   }
   
   public static BufferedImage toImage() {
-    return toImage(0, 0, 31, 23);
+    x1 = 0; y1 = 0; x2 = 31; y2 = 23;
+    return toImagePart();
   }
   
-  public static BufferedImage toImage(int x1, int y1, int x2, int y2) {
-    BufferedImage image = new BufferedImage(256, 192
+  public static BufferedImage toImagePart() {
+    int dx = x2 - x1 + 1, dy = y2 - y1 + 1;
+    BufferedImage image = new BufferedImage(dx << 3, dy << 3
         , BufferedImage.TYPE_INT_RGB);
     int[] col = new int[2];
-    for(int y = y1; y <= y2; y++) {
-      int lineStart = y << 5;
+    for(int y = 0; y < dy; y++) {
+      int lineStart = (y + y1) << 5;
       int yPos = y << 3;
-      for(int x = x1; x <= x2; x++) {
-        int addr = lineStart | x;
+      for(int x = 0; x < dx; x++) {
+        int addr = lineStart | (x + x1);
         int attr = attrs[addr];
         col[0] = color[(attr >> 3) & 7];
         col[1] = color[attr & 7];
@@ -86,14 +90,61 @@ public class Main {
     return image;
   }
 
-  public static void saveImage(String fileName) throws IOException {
-    File outputfile = new File(fileName);
-    ImageIO.write(toImage(), "png", outputfile);
+  public static void saveImage(int num) throws IOException {
+    File outputfile = new File("D:/temp2/output/"
+        + String.format("%08d", num) + ".png");
+    ImageIO.write(toImagePart(), "png", outputfile);
   }
   
   public static class ChunkList extends LinkedList<Chunk> {};
   
   public static Chunk[] background = new Chunk[768];
+  
+  public static int x1, x2, y1, y2;
+  
+  public static boolean usedBlock(int i) {
+    if(chunks[i] == zeroChunk) return true;
+    
+    if(chunks[i].equals(background[i])) {
+      chunks[i] = zeroChunk;
+      return true;
+    }
+    
+    if(attrs[i] == 0b001111) return true;
+    
+    attrs[i] = 0b001111;
+    
+    return false;
+  }
+  
+  public static boolean findBlock(int i) {
+    if(usedBlock(i)) return false;
+    
+    x1 = x2 = i & 31;
+    y1 = y2 = i >> 5;
+    
+    spawn(x1, y1);
+    
+    return true;
+  }
+  
+  public static void spawn(int x, int y) {
+    x1 = Integer.min(x1, x);
+    y1 = Integer.min(y1, y);
+    x2 = Integer.max(x2, x);
+    y2 = Integer.max(y2, y);
+    
+    if(x > 0) check(x - 1, y);
+    if(y > 0) check(x, y - 1);
+    if(x < 31) check(x + 1, y);
+    if(y < 23) check(x, y + 1);
+  }
+  
+  public static void check(int x, int y) {
+    int i = x + (y << 5);
+    if(usedBlock(i)) return;
+    spawn(x, y);
+  }
   
   public static void main(String[] args) {
     int from = 754, to = 912;
@@ -129,23 +180,18 @@ public class Main {
         background[i] = maxChunk;
       }
       
-      Chunk zeroChunk = new Chunk();
+      int outnum = 0;
       for(int num = from; num <= to; num++) {
         loadScreen(num);
         for(int i = 0; i < 768; i++) {
-          if(chunks[i].equals(background[i])) {
-            chunks[i] = zeroChunk;
-          } else {
-            attrs[i] = 0b001111;
+          if(findBlock(i)) {
+            outnum++;
+            saveImage(outnum);
           }
-          //chunks[i].add(background[i]);
         }
-        
-        saveImage("D:/temp2/output/" + String.format("%08d", num) + ".png");
       }
-      
     } catch (IOException e) {
-      int a = 0;
+      System.err.println("I/O error");
     }
   }
 }
