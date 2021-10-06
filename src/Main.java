01202b094bfe21781da218626a65a1bdcdc0c5c8
Matvey Doomchev <32887734+doomchev@users.noreply.github.com>
2021-10-06 18:13:05 +1100

    version 0.2
    Conversion to more convenient screen format.
    Separated code for loading and saving png image.

--- src/Main.java
+++ src/Main.java
@@ -6,46 +6,60 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Main {
-  public static void main(String[] args) {
-    try {
-      int[] color = new int[8];
-      color[0] = 0x000000;
-      color[1] = 0x0000FF;
-      color[2] = 0xFF0000;
-      color[3] = 0xFF00FF;
-      color[4] = 0x00FF00;
-      color[5] = 0x00FFFF;
-      color[6] = 0xFFFF00;
-      color[7] = 0xFFFFFF;
-      
-      File source = new File("source/image-00000001.scr");
-      FileInputStream input = new FileInputStream(source);
-      byte[] byteScreen = new byte[6912];
-      input.read(byteScreen);
-      int[] screen = new int[6912];
-      for(int i = 0; i < 6912; i++) screen[i] = byteScreen[i] & 0xFF;
-      
-      BufferedImage image = new BufferedImage(256, 192
-          , BufferedImage.TYPE_INT_RGB);
-      int[] col = new int[2];
-      for(int y = 0; y < 192; y++) {
-        int lineStart = ((((y >> 3) & 0b111) | ((y & 0b111) << 3)
-            | (y & 0b111000000)) << 5);
-        for(int x = 0; x < 32; x++) {
-          int attr = screen[6144 | ((y >> 3) << 5) | x];
-          col[0] = color[(attr >> 3) & 7];
-          col[1] = color[attr & 7];
-          int val = screen[lineStart | x];
-          int xx = x << 3;
-          for(int i = 7; i >= 0; i--) {
-            image.setRGB(xx | i, y, col[val & 1]);
-            val = val >> 1;
+  private static final int[] screen = new int[6144], attrs = new int[768]
+      , color = {0x000000, 0x0000FF, 0xFF0000, 0xFF00FF
+          , 0x00FF00, 0x00FFFF, 0xFFFF00, 0xFFFFFF};
+    
+  public static void loadScreen(int num) throws IOException {
+    File source = new File("source/image-" + String.format("%08d", num)
+        + ".scr");
+    FileInputStream input = new FileInputStream(source);
+    byte[] byteScreen = new byte[6912];
+    input.read(byteScreen);
+    for(int x = 0; x < 768; x++) attrs[x] = byteScreen[6144 | x] & 0xFF;
+    for(int part = 0; part < 3; part++) {
+      int partPos = part << 11;
+      for(int yy = 0; yy < 8; yy++) {
+        int yySource = partPos + (yy << 8);
+        int yyDest = partPos + (yy << 5);
+        for(int y = 0; y < 8; y++) {
+          int ySource = yySource + (y << 5);
+          int yDest = yyDest + (y << 8);
+          for(int x = 0; x < 32; x++) {
+            screen[yDest | x] = byteScreen[ySource | x] & 0xFF;
           }
         }
       }
-      
-      File outputfile = new File("D:/temp2/image.png");
-      ImageIO.write(image, "png", outputfile);
+    }
+  }
+  
+  public static void saveImage(String fileName) throws IOException {
+    BufferedImage image = new BufferedImage(256, 192
+        , BufferedImage.TYPE_INT_RGB);
+    int[] col = new int[2];
+    for(int y = 0; y < 192; y++) {
+      int lineStart = y << 5;
+      for(int x = 0; x < 32; x++) {
+        int attr = attrs[((y >> 3) << 5) | x];
+        col[0] = color[(attr >> 3) & 7];
+        col[1] = color[attr & 7];
+        int val = screen[lineStart | x];
+        int xx = x << 3;
+        for(int i = 7; i >= 0; i--) {
+          image.setRGB(xx | i, y, col[val & 1]);
+          val = val >> 1;
+        }
+      }
+    }
+
+    File outputfile = new File(fileName);
+    ImageIO.write(image, "png", outputfile);
+  }
+  
+  public static void main(String[] args) {
+    try {
+      loadScreen(1);
+      saveImage("D:/temp2/image.png");
     } catch (IOException e) {
       
     }
