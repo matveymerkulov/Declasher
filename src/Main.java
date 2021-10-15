1349d3ab2a9f1526e9a5ecffd7964899bae769ea
Matvey Doomchev <32887734+doomchev@users.noreply.github.com>
2021-10-15 21:48:05 +1100

    0.11 screen switch detection
    Detection of screen switch based on attributes change.
    Minimum quantity of blocks with changed attributes.
    Minimum frames value to discard garbage.
    Pixel will be switched on for background now when percent of frames with it turned on is more than defined (old was always 50%).

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import java.util.LinkedList;
 class Main {
   public static final int BORDER_SIZE = 4, MAX_DISTANCE = 2
       , MIN_WIDTH = 8, MAX_WIDTH = 24, MIN_HEIGHT = 8, MAX_HEIGHT = 32
-      , AREA_X = 0, AREA_Y = 6, AREA_WIDTH = 32, AREA_HEIGHT = 18;
+      , AREA_X = 0, AREA_Y = 6, AREA_WIDTH = 32, AREA_HEIGHT = 18
+      , MAX_DIFFERENCE = 8, MIN_FRAMES = 20;
+  public static final double PERCENT_ON = 0.7;
+      
   public boolean colored = false;
   
   public static final int [] color = {0x000000, 0x0000FF, 0xFF0000, 0xFF00FF
@@ -14,8 +17,8 @@ class Main {
   
   public static void main(String[] args) {
     try {
-      Screen.process(216, 216);
-      Screen.saveBackground();
+      Screen.process(1, 2966, "source/image-000");
+      //Screen.process(1, 60299, "D:/temp2/scr/");
       //for(Image image: images) image.save();
     } catch (IOException e) {
       System.err.println("I/O error");
--- src/Screen.java
+++ src/Screen.java
@@ -2,6 +2,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import javax.imageio.ImageIO;
 
 public class Screen extends Main {
@@ -10,23 +11,22 @@ public class Screen extends Main {
       , AREA_SIZE = AREA_WIDTH * AREA_HEIGHT, PIXEL_SIZE = AREA_SIZE << 6
       , MAX_AREA_X = AREA_X + AREA_WIDTH, MAX_AREA_Y = AREA_Y + AREA_HEIGHT;
   
-  private static int[] background0, background1;
-  private static final int[] attrs = new int[ATTR_SIZE];
-  private static final boolean[] background = new boolean[PIXEL_SIZE];
+  private static final int[] backgroundOn = new int[PIXEL_SIZE];
+  private static int[] attrs, backgroundAttrs;
 
-  private static boolean[] load(int num, boolean withAttributes)
+  private static boolean[] load(String path, int num)
       throws IOException {
     boolean[] data = new boolean[PIXEL_SIZE];
     
-    File image = new File("source/image-" + String.format("%08d", num)
+    File image = new File(path + String.format("%05d", num)
         + ".scr");
     FileInputStream input = new FileInputStream(image);
     byte[] byteScreen = new byte[BYTE_SIZE + ATTR_SIZE];
     input.read(byteScreen);
     
-    if(withAttributes)
-      for(int x = 0; x < ATTR_SIZE; x++)
-        attrs[x] = byteScreen[BYTE_SIZE | x] & 0xFF;
+    attrs = new int[ATTR_SIZE];
+    for(int x = 0; x < ATTR_SIZE; x++)
+      attrs[x] = byteScreen[BYTE_SIZE | x] & 0xFF;
     
     main: for(int part = 0; part < 3; part++) {
       int partSource = part << 11;
@@ -43,10 +43,12 @@ public class Screen extends Main {
           for(int x = 0; x < AREA_WIDTH; x++) {
             int source = yySource | (x + AREA_X);
             int destination = (yyDestination + x) << 3;
-            int value = byteScreen[source] & 0xFF;
+            int byteValue = byteScreen[source] & 0xFF;
             for(int xx = 7; xx >= 0; xx--) {
-              data[destination | xx] = (value & 1) > 0;
-              value = value >> 1;
+              int addr = destination | xx;
+              boolean pixelValue = (byteValue & 1) > 0;
+              data[addr] = pixelValue;
+              byteValue = byteValue >> 1;
             }
           }
         }
@@ -56,36 +58,57 @@ public class Screen extends Main {
     return data;
   }
   
-  public static void loadBackground(int num, boolean withAttributes)
-      throws IOException {
-    boolean[] screen = load(num, withAttributes);
-    for(int i = 0; i < PIXEL_SIZE; i++)
-      if(screen[i]) background1[i]++; else background0[i]++;
+  public static int difference() {
+    int difference = 0;
+    for(int i = 0; i < ATTR_SIZE; i++)
+      if(attrs[i] != backgroundAttrs[i]) difference++;
+    return difference;
   }
 
-  public static void process(int from, int to) throws IOException {
-    background0 = new int[PIXEL_SIZE];
-    background1 = new int[PIXEL_SIZE];
-    for(int num = from; num <= to; num++)
-      loadBackground(num, num == from);
-    for(int i = 0; i < PIXEL_SIZE; i++)
-      background[i] = background1[i] >= background0[i];
+  public static void process(int from, int to, String path) throws IOException {
+    int frames = 0;
+    boolean newSection = true;
+    for(int num = from; num <= to; num++) {
+      boolean[] screen = load(path, num);
+      if(!newSection && difference() > MAX_DIFFERENCE) {
+        if(frames >= MIN_FRAMES) saveBackground(frames);
+        frames = 0;
+        newSection = true;
+      }
+      
+      if(newSection) {
+        //background = screen;
+        Arrays.fill(backgroundOn, 0);
+        newSection = false;
+      }
+      
+      backgroundAttrs = attrs;
+      frames++;
+      for(int i = 0; i < PIXEL_SIZE; i++)
+        if(screen[i]) backgroundOn[i]++;
+    }
   }
   
-  public static void saveBackground() throws IOException {
+  private static int fileNumber = -1;
+      
+  public static void saveBackground(int frames) throws IOException {
     BufferedImage image = new BufferedImage(PIXEL_WIDTH, PIXEL_HEIGHT
         , BufferedImage.TYPE_INT_RGB);
     
+    int minFrames = (int) Math.floor(PERCENT_ON * frames);
     for(int y = 0; y < PIXEL_HEIGHT; y++) {
       int ySource = y << 8;
       int yAttrSource = ((y >> 3) + AREA_Y) << 5;
       for(int x = 0; x < PIXEL_WIDTH; x++) {
-        int attr = attrs[yAttrSource | (x >> 3 + AREA_X)];
-        boolean value = background[ySource | x];
+        int attr = backgroundAttrs[yAttrSource | (x >> 3 + AREA_X)];
+        int addr = ySource | x;
+        boolean value = backgroundOn[addr] >= minFrames;
         image.setRGB(x, y, value ? color[attr & 7] : color[(attr >> 3) & 7]);
       }
     }
     
-    File outputfile = new File("D:/temp2/output/background.png");
+    fileNumber++;
+    File outputfile = new File("D:/temp2/output/"
+        + String.format("%06d", fileNumber) +".png");
     ImageIO.write(image, "png", outputfile);  }
 }
